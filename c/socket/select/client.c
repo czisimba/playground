e1d08e3aea2084a61ac1524109af1510341680d1
程志 <zhi.cheng@zerotech.com>
2016-05-05 16:03:33 +0800

    test select multi client

--- c/socket/select/client.c
+++ c/socket/select/client.c
@@ -2,15 +2,16 @@
 #include <string.h>
 #include <sys/socket.h>
 #include <sys/types.h>
+#include <arpa/inet.h>
 #include <unistd.h>
 #include <netinet/in.h>
-#include <syslog.h>
 #include <errno.h>
 #include <stdlib.h>
 
 #define MAX_LISTEN_NUM 5
 #define SEND_BUF_SIZE 100
-#define SERVER_PORT 11010
+#define SERVER_PORT_START 11010
+#define MAX_CONNECT_SOCKET 100
 int main()
 {
     int sock_fd = 0;
@@ -19,40 +20,46 @@ int main()
     int leftlen = 0;
     char *ptr = NULL;
     struct sockaddr_in ser_addr;
+    short server_port = SERVER_PORT_START;
+    int i;
 
-    memset(&ser_addr, 0, sizeof(ser_addr));
-    ser_addr.sin_family = AF_INET;
-    inet_aton("127.0.0.1", (struct in_addr *)&ser_addr.sin_addr);
-    ser_addr.sin_port = htons(SERVER_PORT);
-    sock_fd = socket(AF_INET, SOCK_STREAM, 0);
-    if(sock_fd < 0)
-    {
-        syslog(LOG_ERR, "%s:%d, create socket failed", __FILE__, __LINE__);
-        exit(1);
-    }
-    if(connect(sock_fd, (struct sockaddr *)&ser_addr, sizeof(ser_addr)) < 0)
-    {
-        syslog(LOG_ERR, "%s:%d, connect socket failed", __FILE__, __LINE__);
-        exit(1);
-    }
-    //send data
-    sprintf(sendbuf, "hello i am client %d", getpid());
-    leftlen = strlen(sendbuf) + 1;
-    ptr = sendbuf;
-    while(leftlen)
-    {
-        retlen = send(sock_fd, ptr, leftlen, 0);
-        if(retlen < 0)
+    for (i = 0;i < MAX_CONNECT_SOCKET;i++) {
+        memset(&ser_addr, 0, sizeof(ser_addr));
+        ser_addr.sin_family = AF_INET;
+        inet_aton("127.0.0.1", (struct in_addr *)&ser_addr.sin_addr);
+        ser_addr.sin_port = htons(server_port++);
+        sock_fd = socket(AF_INET, SOCK_STREAM, 0);
+        if(sock_fd < 0)
         {
-            if(errno == EINTR)
-                retlen = 0;
-            else
-                exit(1);
+            printf("%s:%d, create socket failed\n", __FILE__, __LINE__);
+            exit(1);
         }
-        leftlen -= retlen;
-        ptr += retlen;
+        if(connect(sock_fd, (struct sockaddr *)&ser_addr, sizeof(ser_addr)) < 0)
+        {
+            printf("%s:%d, connect socket failed\n", __FILE__, __LINE__);
+            exit(1);
+        }
+        //send data
+        sprintf(sendbuf, "hello i am client %d", getpid());
+        printf("port %d connect\n", server_port);
+        leftlen = strlen(sendbuf) + 1;
+        ptr = sendbuf;
+        while(leftlen)
+        {
+            retlen = send(sock_fd, ptr, leftlen, 0);
+            if(retlen < 0)
+            {
+                if(errno == EINTR)
+                    retlen = 0;
+                else
+                    exit(1);
+            }
+            leftlen -= retlen;
+            ptr += retlen;
+        }
+        close(sock_fd);
+        usleep(100 * 1000);
     }
-    close(sock_fd);
 
     return 0;
 
--- c/socket/select/server.c
+++ c/socket/select/server.c
@@ -14,46 +14,56 @@
 #define MAX_LISTEN_NUM 5
 #define SEND_BUF_SIZE 100
 #define RECV_BUF_SIZE 100
-#define LISTEN_PORT 11010
+#define LISTEN_PORT_START 11010
+#define MAX_LISTEN_SOCKET 100
 
 int main()
 {
-    int listen_sock = 0, app_sock = 0;
+    int sock[MAX_LISTEN_SOCKET], max_sock_fd = 0;
     struct sockaddr_in hostaddr, clientaddr;
     struct timeval timeout;
     fd_set rds;
     char recvbuf[RECV_BUF_SIZE] = {0};
     int retlen = 0, leftlen = 0;
     char *ptr = NULL;
+    int i;
+    short listen_port = LISTEN_PORT_START;
 
-    memset((void *)&hostaddr, 0, sizeof(hostaddr));
+    FD_ZERO(&rds);
     memset((void *)&clientaddr, 0, sizeof(clientaddr));
-    hostaddr.sin_family = AF_INET;
-    hostaddr.sin_port = htons(LISTEN_PORT);
-    hostaddr.sin_addr.s_addr = htonl(INADDR_ANY);//auto get system ip address
-    listen_sock = socket(AF_INET, SOCK_STREAM, 0);
-    if(listen_sock < 0) {
-        perror("%s:%d, create socket failed");
-        exit(1);
+    for (i = 0;i < MAX_LISTEN_SOCKET;i++) {
+        memset((void *)&hostaddr, 0, sizeof(hostaddr));
+        hostaddr.sin_family = AF_INET;
+        hostaddr.sin_port = htons(listen_port++);
+        hostaddr.sin_addr.s_addr = htonl(INADDR_ANY);//auto get system ip address
+        sock[i] = socket(AF_INET, SOCK_STREAM, 0);
+        if (sock[i] < 0) {
+            perror("%s:%d, create socket failed");
+            exit(1);
+        }
+        if (bind(sock[i], (struct sockaddr *)&hostaddr, sizeof(hostaddr)) < 0)
+        {
+            perror("%s:%d, bind socket failed");
+            exit(1);
+        }
+        if (listen(sock[i], MAX_LISTEN_NUM) < 0)
+        {
+            syslog(LOG_ERR, "%s:%d, listen failed", __FILE__, __LINE__);
+            exit(1);
+        }
+        FD_SET(sock[i], &rds);
+        printf("i:%d sock:%d\n", i, sock[i]);
+        if (sock[i] > max_sock_fd)
+            max_sock_fd = sock[i];
+        printf("max_sock_fd:%d\n", max_sock_fd);
     }
-    if(bind(listen_sock, (struct sockaddr *)&hostaddr, sizeof(hostaddr)) < 0)
-    {
-        perror("%s:%d, bind socket failed");
-        exit(1);
-    }
-    if(listen(listen_sock, MAX_LISTEN_NUM) < 0)
-    {
-        syslog(LOG_ERR, "%s:%d, listen failed", __FILE__, __LINE__);
-        exit(1);
-    }
+
     while(1)
     {
-        FD_ZERO(&rds);
-        FD_SET(listen_sock, &rds);
         timeout.tv_sec  = 5;
         timeout.tv_usec = 0;
 
-        int ret = select(listen_sock+1, &rds, NULL, NULL, &timeout);
+        int ret = select(max_sock_fd+1, &rds, NULL, NULL, NULL);
         if ( ret == 0 ) {
             printf("Time Expired\n");
             continue;
@@ -62,33 +72,37 @@ int main()
             continue;
         } 
         printf("Socket select num:%d\n", ret);
-        if (FD_ISSET(listen_sock, &rds)) {
-            app_sock = accept(listen_sock, NULL, NULL);
-            if(app_sock < 0)
-            {
-                printf("%s:%d, accept failed", __FILE__, __LINE__);
-                exit(1);
-            }
-            //receive data
-            ptr = recvbuf;
-            leftlen = RECV_BUF_SIZE -1;
-            {
-                retlen = recv(app_sock, ptr, leftlen, 0) ;
-                if(retlen < 0)
+        for (i = 0;i < MAX_LISTEN_SOCKET;i++) {
+            if (FD_ISSET(sock[i], &rds)) {
+                int app_sock = accept(sock[i], NULL, NULL);
+                if(app_sock < 0)
                 {
-                    if(errno == EINTR)
-                        retlen = 0;
-                    else
-                        exit(1);
+                    printf("%s:%d, accept failed", __FILE__, __LINE__);
+                    exit(1);
                 }
-                leftlen -= retlen;
-                ptr += retlen;
+                printf("accept socket fd: %d\n", sock[i]);
+                //receive data
+                ptr = recvbuf;
+                leftlen = RECV_BUF_SIZE -1;
+                {
+                    retlen = recv(app_sock, ptr, leftlen, 0) ;
+                    if(retlen < 0)
+                    {
+                        if(errno == EINTR)
+                            retlen = 0;
+                        else
+                            exit(1);
+                    }
+                    leftlen -= retlen;
+                    ptr += retlen;
+                }
+                printf("i is %d\n,receive data is : %s\n", i, recvbuf);
+                close(app_sock);
             }
-            printf("receive data is : %s\n", recvbuf);
-            close(app_sock);
         }
     }
-    close(listen_sock);
+    for (i = 0;i < MAX_LISTEN_SOCKET;i++)
+        close(sock[i]);
 
     return 0;
 }
